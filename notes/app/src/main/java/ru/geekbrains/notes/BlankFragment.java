b054cbe536e70c669484ad6210b131e92d277053
OlgaMArsal <inconcretehome@gmail.com>
2021-02-01 19:06:06 +0300

    notes - lesson 6

--- notes/app/src/main/java/ru/geekbrains/notes/BlankFragment.java
+++ notes/app/src/main/java/ru/geekbrains/notes/BlankFragment.java
@@ -1,57 +1,54 @@
 package ru.geekbrains.notes;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BlankFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class BlankFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private boolean isLandscape;
 
     public BlankFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BlankFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BlankFragment newInstance(String param1, String param2) {
+    public static BlankFragment newInstance() {
         BlankFragment fragment = new BlankFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        isLandscape = getResources().getConfiguration().orientation
+                = Configuration.ORIENTATION_LANDSCAPE;
+
+        if(isLandscape) {
+            showLandBlankFragmentTwo(0);
+        }
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
@@ -60,5 +57,60 @@ public class BlankFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_blank, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initList(view);
+    }
+
+    private void initList(View view) {
+        LinearLayout layoutView = (LinearLayout)view;
+        String[] notes = getResources().getStringArray(R.array.notes);
+
+        Context context = getContext();
+        for (int i = 0; i < notes.length; i++) {
+            if (context != null) {
+                String note = notes[i];
+                TextView textView = new TextView(context);
+                textView.setText(note);
+                textView.setTextSize(30);
+                layoutView.addView(textView);
+                final int fi = i;
+                textView.setOnClickListener(v -> showBlankFragmentTwo(fi));
+            }
+        }
+    }
+
+    private void showBlankFragmentTwo(int index) {
+        if (isLandscape) {
+            showLandBlankFragmentTwo(index);
+        } else {
+            showPortBlankFragmentTwo(index);
+        }
+    }
+
+    private void showPortBlankFragmentTwo(int index) {
+        Context context = getContext();
+        if (context != null) {
+            Intent intent = new Intent(getActivity(), BlankFragmentTwo.class);
+            intent.putExtra(BlankFragmentTwo.ARG_INDEX, index);
+            startActivity(intent);
+        }
+    }
+
+
+    private void showLandBlankFragmentTwo(int index) {
+        BlankFragmentTwo detail = BlankFragmentTwo.newInstance(index);
+
+        FragmentActivity context = getActivity();
+        if (context != null) {
+            FragmentManager fragmentManager = context.getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.editTextOne, detail);
+            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            fragmentTransaction.commit();
+        }
     }
 }
